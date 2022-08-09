93ffbeffd406447e759404ff6881b40ac68144e5
tsm1103248 <tsm1103248@gmail.com>
2022-08-09 15:16:28 +0300

    09.08.2022

--- src/HomeWork4.java
+++ src/HomeWork4.java
@@ -1,13 +1,26 @@
 import java.util.Scanner;
 public class HomeWork4 {
-    public static void main (String[]args){
-        int[] myArray = new int[100];
+    public static void main(String[] args) {
+        int[] myArray = new int[10];
+        for (int i = 0; i < myArray.length; i++) {
+            myArray[i] = (int) (Math.random() * 10);
+            System.out.println(myArray[i]);
+        }
+        System.out.println("Enter an integer from 0 to 20");
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        if (t=i) {
 
-    }
+                            System.out.println("Match");
+                }
+            }
+
+
+        }
 
 
 
 
 
 
-}
+
